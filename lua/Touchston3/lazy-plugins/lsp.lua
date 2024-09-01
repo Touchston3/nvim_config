84a4aee87c38e43ba997c28db7942897fc8b2af7
touchston3 <touchston3.primary@gmail.com>
2024-09-01 11:25:06 -0500

    added more basic lsp functionality

--- lua/Touchston3/lazy-plugins/lsp.lua
+++ lua/Touchston3/lazy-plugins/lsp.lua
@@ -33,5 +33,11 @@ return { -- LSP Configuration & Plugins
 			ensure_installed = {
 			},
 		})
+
+
+		vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+		vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+		vim.keymap.set("n", "<leader>gr", require("telescope.builtin").lsp_references, {})
 	end
 }
